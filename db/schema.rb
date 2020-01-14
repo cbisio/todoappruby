a7837116eff168e2702bedc6a60f4a638e859bb3
Xavi <xavi@geekshubs.com>
2020-01-14 12:45:45 +0100

    created item model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_112313) do
+ActiveRecord::Schema.define(version: 2020_01_14_113953) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.boolean "done"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
