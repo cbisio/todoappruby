e2368795756f144d160695150770b999066b41a0
Xavi <xavi@geekshubs.com>
2020-01-15 10:59:48 +0100

    added todos controller post and rspec

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,16 @@
 class TodosController < ApplicationController
+    
+    
     def index
         render json: Todo.all , status: :ok
+    end
+
+    def create
+        render json: Todo.create(create_params), status:  :created
+    end
+
+    private
+    def create_params
+        params.require(:todo).permit(:title,:created_by)
     end
 end
--- spec/request/todos_spec.rb
+++ spec/request/todos_spec.rb
@@ -19,4 +19,35 @@ RSpec.describe 'Todo Api request', type: :request do
             expect(json.size).to eq(5)
         end 
     end
+
+    describe 'POST /todos' do
+        
+        context 'when the request is valid' do
+            req_payload = {
+                todo: {  title: 'Superman', created_by:'xavi'}
+            }
+            
+            before { post '/todos', params:req_payload}
+            it 'return status code 201' do
+                expect(response).to have_http_status(201)
+            end
+            it 'return not empty' do
+                expect(json).not_to be_empty
+            end
+
+            it 'return id = 6' do
+                expect(json['id']).to eq(6)
+            end
+        end
+        context 'when te request is invalid' do
+            
+            req_payload = {
+                todo: { title:'superman'}
+            }
+            before { post '/todos', params:req_payload}
+            it 'returns status code 422' do
+                expect(response).to have_http_status(422)
+            end
+        end
+    end
 end
