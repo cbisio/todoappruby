382ab4027c9fdf2f19fe7bf2bf4c6222f83172a1
Xavi <xavi@geekshubs.com>
2020-01-16 13:59:45 +0100

    authentication_controller create

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,17 +5,25 @@ class AuthenticationController < ApplicationController
     def login
     
             
-         @user = User.find_by(params[:email])
+         @user = User.find_by(email:params[:email])
          print @user.password
          
-         if @user && @user.authenticate('pass')
+         if @user && @user.authenticate(params[:password])
             @token = JwtService::encode([user_id:@user.id,name:@user.name])        
             json_response(token: @token)    
         else
-             Logger.error "No existe usuario"       
+             #Logger.error "No existe usuario"       
          end
        
         
     end
 
+    private
+        def user_params
+            params.permit(
+                :name,
+                :email,
+                :password
+            )
+        end
 end
