7e9116844b1466271a969833784000065882055f
Xavi <xavi@geekshubs.com>
2020-01-20 13:45:41 +0100

    refactor auth rspec fixed solved

--- spec/request/auth_spec.rb
+++ spec/request/auth_spec.rb
@@ -7,26 +7,24 @@ RSpec.describe 'Authentication', type: :request do
 
         context 'When auth request is valid' do
             let!(:user) {create(:user)}
+            let(:credentials) do
+                {
+                    email: user.email,
+                    password: user.password
+                }
             
+            end
+
+
             it 'return status code 200' do
                 
          
-                payload= 
-                    {
-                    email: user.email,
-                    password_digest: user.password_digest
-                }.to_json
-                post '/auth/login', params: payload
+                                    
+                post '/auth/login', params: credentials
                 expect(response).to have_http_status(200)
             end
             it 'returns  token' do
-
-                payload= 
-                    {
-                    email: user.email,
-                    password_digest: user.password_digest
-                }.to_json
-                 post '/auth/login', params: payload
+                 post '/auth/login', params: credentials
                 expect(json['token']).not_to be_nil
             end
         end
